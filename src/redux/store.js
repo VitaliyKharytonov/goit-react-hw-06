f2c7c31450b9865fbbc16e442439d57aa7213910
VitaliyKharytonov <vitalicaxaritonov@gmail.com>
2024-06-06 16:22:29 +0300

    add task

---
+++ src/redux/store.js
@@ -0,0 +1,7 @@
+import { configureStore } from "@reduxjs/toolkit";
+import contactsSlice from "./contactsSlice";
+import filtersSlice from "./filtersSlice";
+
+export const store = configureStore({
+  reducer: { contacts: contactsSlice, filters: filtersSlice },
+});
